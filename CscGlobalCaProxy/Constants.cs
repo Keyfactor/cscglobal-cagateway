2897759b16a9bb35b4ec1c7006b7ea6920e58be3
Brian Hill <bhill@keyfactor.com>
2021-07-23 11:38:18 -0400

    Initial Commit

---
+++ CscGlobalCaProxy/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Keyfactor.AnyGateway.CscGlobal
+{
+    public class Constants
+    {
+        public static string CscGlobalUrl = "CscGlobalUrl";
+        public static string CscGlobalApiKey = "ApiKey";
+        public static string BearerToken = "BearerToken";
+        public static int DefaultPageSize = 100;
+    }
+}
