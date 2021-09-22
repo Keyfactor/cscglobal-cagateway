29fc473fd7eb556b2a8125536f90c5f349454f53
Brian Hill <bhill@keyfactor.com>
2021-09-22 10:42:21 -0400

    Initial Commit

---
+++ CscGlobalCaProxy/Interfaces/ICertificateResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Keyfactor.AnyGateway.CscGlobal.Interfaces
+{
+    public interface ICertificateResponse
+    {
+        string Uuid { get; set; }
+        string CommonName { get; set; }
+        List<string> AdditionalNames { get; set; }
+        string CertificateType { get; set; }
+        string Status { get; set; }
+        string EffectiveDate { get; set; }
+        string ExpirationDate { get; set; }
+        string BusinessUnit { get; set; }
+        string OrderedBy { get; set; }
+        string OrderDate { get; set; }
+        object ServerSoftware { get; set; }
+        string Certificate { get; set; }
+    }
+}
